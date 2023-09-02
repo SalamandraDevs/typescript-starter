ad18c1585022f84ce4b1ce8b69a566fd0712b1d2
OdairTrujillo <haldrik@gmail.com>
2023-09-02 11:55:11 -0500

    Improving tsconfig

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,3 @@
 import { usuario } from 'local-package';
 console.log('Message from main app: ', usuario);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxFQUFFLE9BQU8sRUFBRSxNQUFNLGVBQWUsQ0FBQztBQUV4QyxPQUFPLENBQUMsR0FBRyxDQUFDLHlCQUF5QixFQUFFLE9BQU8sQ0FBQyxDQUFDIn0=
